2ff935e47e925ff1cbcdfe960c466b58b0db1337
Tazo <tazogigitashvili@gmail.com>
2023-04-08 16:08:26 +0400

    Created groups for MVC pattern and root viewcontroller LoginController. Lets start a journey. Enjoy <3

--- App-like-Uber-Firebase-backend/Controller/LoginController.swift
+++ App-like-Uber-Firebase-backend/Controller/LoginController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  App-like-Uber-Firebase-backend
 //
 //  Created by Tazo Gigitashvili on 08.04.23.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-    //First Commit
 }
-
