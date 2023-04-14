9521471feefead5ecd25f8cf075084971d42ef8b
Tazo <tazogigitashvili@gmail.com>
2023-04-14 20:13:59 +0400

    Login UI code refactor. extention of uiview and textfield

--- App-like-Uber-Firebase-backend/Utility/Extentions.swift
+++ App-like-Uber-Firebase-backend/Utility/Extentions.swift
@@ -8,6 +8,46 @@
 import UIKit
 
 extension UIView {
+    
+    func inputContainerView(image: UIImage, textField: UITextField? = nil,
+                            segmentedControl: UISegmentedControl? = nil) -> UIView {
+        let view = UIView()
+        
+        let imageView = UIImageView()
+        imageView.image = image
+        imageView.alpha = 0.87
+        view.addSubview(imageView)
+        
+        
+        if let textField = textField {
+            imageView.centerY(inView: view)
+            imageView.anchor(left: view.leftAnchor, paddingLeft: 8, width: 24, height: 24)
+            
+            view.addSubview(textField)
+            textField.centerY(inView: view)
+            textField.anchor(left: imageView.rightAnchor, bottom: view.bottomAnchor,
+                             right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8)
+        }
+        
+        if let sc = segmentedControl {
+            imageView.anchor(top: view.topAnchor, left: view.leftAnchor,
+                             paddingTop: -8, paddingLeft: 8, width: 24, height: 24)
+            
+            view.addSubview(sc)
+            sc.anchor(left: view.leftAnchor, right: view.rightAnchor,
+                      paddingLeft: 8, paddingRight: 8)
+            sc.centerY(inView: view, constant: 8)
+        }
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .lightGray
+        view.addSubview(separatorView)
+        separatorView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor,
+                             right: view.rightAnchor, paddingLeft: 8, height: 0.75)
+        
+        return view
+    }
+    
     func anchor(top: NSLayoutYAxisAnchor? = nil,
                 left: NSLayoutXAxisAnchor? = nil,
                 bottom: NSLayoutYAxisAnchor? = nil,
@@ -73,6 +113,19 @@ extension UIView {
         layer.shadowOpacity = 0.55
         layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
         layer.masksToBounds = false
+    }
+}
+
+extension UITextField {
+    func textField(withPlaceholder placeholder: String, isSecureTextEntry: Bool) -> UITextField {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.font = UIFont.systemFont(ofSize: 16)
+        tf.textColor = .white
+        tf.keyboardAppearance = .dark
+        tf.isSecureTextEntry = isSecureTextEntry
+        tf.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        return tf
     }
 }
 
