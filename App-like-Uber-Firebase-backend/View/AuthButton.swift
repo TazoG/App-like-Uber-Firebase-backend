49e2843d6a5e57efa02333af1027a8197b6bda96
Tazo <tazogigitashvili@gmail.com>
2023-04-15 12:58:30 +0400

    Created Sign up page

---
+++ App-like-Uber-Firebase-backend/View/AuthButton.swift
@@ -0,0 +1,25 @@
+//
+//  AuthButton.swift
+//  App-like-Uber-Firebase-backend
+//
+//  Created by Tazo Gigitashvili on 15.04.23.
+//
+
+import UIKit
+
+class AuthButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.cornerRadius = 5
+        backgroundColor = .mainBlueTint
+        setTitleColor(.white, for: .normal)
+        heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
