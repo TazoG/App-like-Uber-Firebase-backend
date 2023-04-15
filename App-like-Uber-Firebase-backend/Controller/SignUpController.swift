155720f08c01fd65c744669dc65cef5702aab887
Tazo <tazogigitashvili@gmail.com>
2023-04-15 11:57:04 +0400

    Configured nav controller and created sign up controller

---
+++ App-like-Uber-Firebase-backend/Controller/SignUpController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpController.swift
+//  App-like-Uber-Firebase-backend
+//
+//  Created by Tazo Gigitashvili on 15.04.23.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    //MARK: - #Selectors
+}
