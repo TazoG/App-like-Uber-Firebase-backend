14efef9d5d56073b44a6c97a5b19195c5081e27c
Tazo <tazogigitashvili@gmail.com>
2023-04-18 15:48:25 +0400

    Did location input activation view UI

---
+++ App-like-Uber-Firebase-backend/View/LocationInputActivationView.swift
@@ -0,0 +1,54 @@
+//
+//  LocationInputActivationView.swift
+//  App-like-Uber-Firebase-backend
+//
+//  Created by Tazo Gigitashvili on 18.04.23.
+//
+
+import UIKit
+
+class LocationInputActivationView: UIView {
+
+    //MARK: - Properties
+    
+    private let indicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    private let placeholderLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Where to?"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .darkGray
+        return label
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.55
+        layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+        layer.masksToBounds = false
+        layer.cornerRadius = 7
+//        layer.borderWidth = 2
+//        layer.borderColor = UIColor.blue.cgColor
+        
+        addSubview(indicatorView)
+        indicatorView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 16)
+        indicatorView.setDimensions(height: 6, width: 6)
+        
+        addSubview(placeholderLabel)
+        placeholderLabel.centerY(inView: self, leftAnchor: indicatorView.rightAnchor, paddingLeft: 20)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
