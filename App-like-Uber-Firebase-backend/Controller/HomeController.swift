eecba276dd73e13b1e9249b4998ebf2cc1cbb754
Tazo <tazogigitashvili@gmail.com>
2023-05-16 20:15:48 +0400

    configured destination data for ride action view title and address labels

--- App-like-Uber-Firebase-backend/Controller/HomeController.swift
+++ App-like-Uber-Firebase-backend/Controller/HomeController.swift
@@ -226,8 +226,13 @@ class HomeController: UIViewController {
         }, completion: completion)
     }
     
-    func animateRideActionView(shouldShow: Bool) {
+    func animateRideActionView(shouldShow: Bool, destination: MKPlacemark? = nil) {
         let yOrigin = shouldShow ? self.view.frame.height - self.rideActionViewHeight : self.view.frame.height
+        
+        if shouldShow {
+            guard let destination = destination else { return }
+            rideActionView.destination = destination
+        }
         
         UIView.animate(withDuration: 0.3) {
             self.rideActionView.frame.origin.y = yOrigin
@@ -407,7 +412,7 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
             let annotations = self.mapView.annotations.filter({ !$0.isKind(of: DriverAnnotation.self) })
             self.mapView.showAnnotations(annotations, animated: true)
             
-            self.animateRideActionView(shouldShow: true)
+            self.animateRideActionView(shouldShow: true, destination: selectedPlacemarks)
         }
     }
 }
