5c80e99090f182bd0df118ba4eb47df6ea270868
Tazo <tazogigitashvili@gmail.com>
2023-05-22 17:47:13 +0400

    Polyline to pickup location and ride action config done, enum RideActionViewConfiguration, ButtonAction

--- App-like-Uber-Firebase-backend/View/RideActionView.swift
+++ App-like-Uber-Firebase-backend/View/RideActionView.swift
@@ -12,8 +12,43 @@ protocol RideActionViewDelegate: AnyObject {
     func uploadTrip(_ view: RideActionView)
 }
 
-class RideActionView: UIView {
+enum RideActionViewConfiguration {
+    case requestRide
+    case tripAccepted
+    case driverArrived
+    case pickupPassenger
+    case tripInProgress
+    case endTrip
+    
+    init() {
+        self = .requestRide
+    }
+}
 
+enum ButtonAction: CustomStringConvertible {
+    case requestRide
+    case cancel
+    case getDirections
+    case pickup
+    case dropOff
+    
+    var description: String {
+        switch self {
+        case .requestRide: return "CONFIRM UBERX"
+        case .cancel: return "CANCEL RIDE"
+        case .getDirections: return "GET DIRECTIONS"
+        case .pickup: return "PICKUP PASSENGER"
+        case .dropOff: return "DROP OFF PASSENGER"
+        }
+    }
+    
+    init() {
+        self = .requestRide
+    }
+}
+
+class RideActionView: UIView {
+    
     //MARK: - Properties
     
     var destination: MKPlacemark? {
@@ -23,6 +58,8 @@ class RideActionView: UIView {
         }
     }
     
+    var config = RideActionViewConfiguration()
+    var buttonAction = ButtonAction()
     weak var delegate: RideActionViewDelegate?
     
     private let titleLabel: UILabel = {
